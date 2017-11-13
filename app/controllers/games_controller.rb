357e8c90cc3dfa721675a9bc086f28f5899762f4
Chase Paules <cpaules@gmail.com>
2017-11-13 00:19:46 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,5 @@
 class GamesController < ApplicationController
 
-  def index
-    @games = Game.all
-  end
-
-  # def inventory
-  #   product = Product.find(params[:id])
-  #   render plain: product.inventory > 0 ? true : false
-  # end
-  #
-  # def description
-  #   product = Game.find(params[:id])
-  #   render plain: product.description
-  # end
-
   def create
     @game = Game.create(state: params[:state])
     render json: @game
