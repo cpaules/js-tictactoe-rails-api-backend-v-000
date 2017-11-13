1e1a9db26ab41e82838ba16359d9cbc136dc450c
Chase Paules <cpaules@gmail.com>
2017-11-13 00:12:08 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,35 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+
+  def index
+    @games = Game.all
+  end
+
+  # def inventory
+  #   product = Product.find(params[:id])
+  #   render plain: product.inventory > 0 ? true : false
+  # end
+  #
+  # def description
+  #   product = Game.find(params[:id])
+  #   render plain: product.description
+  # end
+
+  def create
+    Game.create(game_params)
+    redirect_to games_path
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    respond_to do |format|
+      format.json { render json: @game}
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:state)
+  end
 
 end
